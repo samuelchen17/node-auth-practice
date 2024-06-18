3fa3d13a85e02bc9a1489d161342a4ab1a3040ee
Samuel Chen <sche842@gmail.com>
2024-06-18 14:56:28 +1000

    errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
 const User = require("../models/userModel");
 
+// handle erros
+const handleErrors = (err) => {
+  // console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+  console.log(err);
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    // use object.values to grab the values from the erros object as an array
+    // Object.values(err.errors).forEach((error) => {
+    //   console.log(errors.properties);
+    // });
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -16,8 +35,8 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send("error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
