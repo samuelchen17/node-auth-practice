ff99e63efaed59b6217af3fe67c7bf6679ef669f
Samuel Chen <sche842@gmail.com>
2024-06-18 19:47:09 +1000

    cookie prac

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 // start an express app
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 // middleware, use static middleware for CSS and images
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine, register the view engine
 app.set("view engine", "ejs");
@@ -29,3 +31,21 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 10,
+    secure: true,
+  });
+
+  res.send("you got the cookie");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+});
