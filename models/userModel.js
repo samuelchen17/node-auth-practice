81a4033b799a205edc60a2a25c59927f47c950c8
Samuel Chen <sche842@gmail.com>
2024-06-18 16:34:20 +1000

    password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,7 +17,32 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// hash password
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 // the `user` here is important, must match the data base "users"
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
+
+// // fire a function after doc saved to db
+// // note: this is not a post request, this is post as in after
+// // after "save" execute the function
+// userSchema.post("save", function (doc, next) {
+//   console.log("new user was created and saved", doc);
+
+//   // next has to be called at the end of every mongoose hook or middleware
+//   next();
+// });
+
+// // fire function before doc saved to db
+// userSchema.pre("save", function (next) {
+//   // using a normal function here so "this" can be used
+//   // this refers to the user instance locally before it is saved to the db
+//   console.log("user about to be created and saved", this);
+//   next();
+// });
